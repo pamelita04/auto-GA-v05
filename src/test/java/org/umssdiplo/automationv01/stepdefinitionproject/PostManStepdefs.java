48bd3c4c7daa2d62c1189ccb0724ce894110f45a
Pame Arnez <wen.pame04@gmail.com>
2019-04-21 19:01:18 -0400

    homework postman post

--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/PostManStepdefs.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/PostManStepdefs.java
@@ -3,6 +3,7 @@ package org.umssdiplo.automationv01.stepdefinitionproject;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.umssdiplo.automationv01.core.customrestassure.HandleRequest;
@@ -14,6 +15,7 @@ import static org.testng.Assert.assertEquals;
  */
 public class PostManStepdefs {
     private Response response;
+    private String resourcePost;
 
     @Given("^GET \"([^\"]*)\" postman endpoint is configured$")
     public void getPostmanEndpointIsConfigured(String headerEndpoint) throws Throwable {
@@ -22,18 +24,20 @@ public class PostManStepdefs {
 
     @Then("^the status code should be (\\d+)$")
     public void theStatusCodeShouldBe(int statusCode) throws Throwable {
+        System.out.println("----response----" + response.jsonPath().get("headers"));
         assertEquals(response.getStatusCode(), statusCode);
     }
 
     @Then("^the responde 'data' value is \"([^\"]*)\"$")
-    public void theRespondeDataValueIs(String esperado) throws Throwable {
-        String actual = response.getDataValue();
-
-        Assert.assertEquals(esperado, actual);
+    public void theRespondeDataValueIs(String bodyActual) throws Throwable {
+        response = HandleRequest.post(resourcePost,bodyActual);
+        JsonPath jsonPathEvaluator = response.jsonPath();
+        String bodyEsperado = jsonPathEvaluator.get("data");
+        Assert.assertEquals(bodyActual,bodyEsperado,"Body no equals");
     }
 
     @Given("^POST \"([^\"]*)\" postman endpoint is configured$")
     public void postPostmanEndpointIsConfigured(String arg0) throws Throwable {
-
+        resourcePost = arg0;
     }
 }
