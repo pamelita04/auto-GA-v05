84674fc1f8536e3fd48b692aa42df4790628d502
Pamela Arnez <jasminum@Pamelas-MacBook-Pro.local>
2019-05-22 21:06:25 -0400

    add dependencias - WebDriver

---
+++ src/test/java/org/umssdiplo/automationv01/beans/ChromeWebDriver.java
@@ -0,0 +1,44 @@
+package org.umssdiplo.automationv01.beans;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class ChromeWebDriver extends EventFiringWebDriver {
+	
+	private static final WebDriver webdriver;
+	private static final Logger logger = LoggerFactory.getLogger(ChromeWebDriver.class);
+
+	private static final Thread CLOSE_THREAD = new Thread() {
+		@Override
+		public void run() {
+			webdriver.close();
+		}
+	};
+
+	static {
+		System.setProperty("webdriver.chrome.driver", "src/chromedriver");
+		
+		ChromeOptions capabilities = new ChromeOptions();
+		webdriver = new ChromeDriver(capabilities);
+		
+		Runtime.getRuntime().addShutdownHook(CLOSE_THREAD);
+	}
+
+	public ChromeWebDriver() {
+		super(webdriver);
+	}
+
+	@Override
+	public void close() {
+		if (Thread.currentThread() != CLOSE_THREAD) {
+			logger.info("You shouldn't close this WebDriver. It's shared and will close when the JVM exits.");
+		}
+		super.close();
+	}
+	
+}
--- src/test/java/org/umssdiplo/automationv01/spring/CucumberContext.java
+++ src/test/java/org/umssdiplo/automationv01/spring/CucumberContext.java
@@ -1,13 +1,14 @@
 package org.umssdiplo.automationv01.spring;
 
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
+import org.umssdiplo.automationv01.beans.ChromeWebDriver;
+//import org.umssdiplo.automationv01.beans.FirefoxWebDriver;
 
 @Configuration
 @ContextConfiguration(classes={PropertiesContext.class})
@@ -21,11 +22,11 @@ public class CucumberContext {
 		WebDriver webdriver = null;
 		switch (webbrowser) {
 			case "firefox":
-				webdriver = new FirefoxDriver();
+//				webdriver = new FirefoxWebDriver();
 				break;
 			
 			case "chrome":
-				webdriver = new ChromeDriver();
+				webdriver = new ChromeWebDriver();
 				break;
 		}
 		return webdriver; 
